55c8fc7cc94463423ab9eb57b96a2b112e09a69b
saltedmuse <eliizavetasereda@gmail.com>
2024-06-13 13:46:42 -0400

    hi

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let img = document.querySelector("img");
 // - Make it so that the colors are different than what's used.
 // - Try using a hex color code.
 button.addEventListener("click", function() {
-    body.style.backgroundColor = "black";
-    header.style.color = "white";
+    body.style.backgroundColor = "pink";
+    header.style.color = "lavender";
     img.style.border = "5px solid white";
 });
